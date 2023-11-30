1e7325076ae18fc0621c2b576c17d664f769b314
Lucas Martins <56140144+lmartins54@users.noreply.github.com>
2023-11-29 22:01:35 -0500

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+function submitForm() {
+    // Get form data
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var age = document.getElementById('age').value;
+    var gender = document.getElementById('gender').value;
+
+    // Display form data (you can replace this with your form submission logic)
+    alert('Name: ' + name + '\nEmail: ' + email + '\nAge: ' + age + '\nGender: ' + gender);
+}
